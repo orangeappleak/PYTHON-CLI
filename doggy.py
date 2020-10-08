c93bb7a8a5eb45621cea0cead6d5a4d02783c0ad
orangeappleak <appannagarikarthik.123@gmail.com>
2020-10-08 11:13:42 +0530

    finally understood something about CLICK

--- doggy.py
+++ doggy.py
@@ -3,12 +3,15 @@ import os
 import subprocess
 import sys
 
-@click.command()
-@click.option('--count','-c',default = 1, help = "Number of greetings")
-@click.option('--name','-n', help = "The person to greet")
-@click.option('--info','-i',help = "provides the information for the tool.")
-def hello(count, name):
-    for x in range(count):
-        click.echo("Hello %s!" % name)
-def info():
-    print("This is a CLI utility tool that helps you do werid things, \n it was designed and developed by the great MR.SPOCK himself(A.KARTHIK)")
+@click.group()
+def cli():
+    pass
+@cli.command()
+def hel():
+    for x in range(5):
+        click.echo("Hello DUMBO")
+
+@cli.command()
+@click.option('--name',default="you are an idiot",help = "greets the user.")
+def name(name):
+    click.echo("Hello Idiot, OH shit my bad. HELLO " + name)
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='doggy',
-    version='0.2',
+    version='0.2.3',
     py_modules=['doggy'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        doggy=doggy:hello
+        doggy=doggy:cli
     ''',
 )
